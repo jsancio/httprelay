e442fb958b246e3ca79c640be85879feddf51882
José Armando García Sancio <jsancio@gmail.com>
2015-03-01 09:46:20 -0800

    Add basic session manager

--- httprelay.go
+++ httprelay.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/rand"
 	"encoding/base64"
 	"fmt"
 	"log"
@@ -8,11 +9,17 @@ import (
 	"net/http"
 )
 
+// TODO: we need to make sure that we remove connections after a timeouto of inactive
+
 func main() {
+	createChannel := make(chan createConnection)
+	queryChannel := make(chan queryConnection)
+	go sessionManager(createChannel, queryChannel)
+
 	http.HandleFunc("/cookie", cookie)
-	http.HandleFunc("/proxy", proxy)
-	http.HandleFunc("/read", read)
-	http.HandleFunc("/write", write)
+	http.Handle("/proxy", proxyHandler{createChannel})
+	http.Handle("/read", readHandler{queryChannel})
+	http.Handle("/write", writeHandler{queryChannel})
 
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
@@ -36,97 +43,112 @@ func cookie(writer http.ResponseWriter, request *http.Request) {
 	}
 }
 
-var conn net.Conn = nil
-
-func proxy(writer http.ResponseWriter, request *http.Request) {
-	log.Printf("Handling Request: %s", request)
-
-	setHeaders(writer, request.Header.Get("Origin"))
-
-	localConn, err := net.Dial("tcp", "localhost:22")
-	if err != nil {
-		log.Printf("Error creating ssh connection: %s", err)
-		writer.WriteHeader(500)
-		fmt.Fprintf(writer, "Error creating ssh connection: %s", err)
-	}
-
-	conn = localConn
-
-	fmt.Fprint(writer, "some_secret")
+type proxyHandler struct {
+	createChannel chan createConnection
 }
 
-func read(writer http.ResponseWriter, request *http.Request) {
+func (self proxyHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	log.Printf("Handling Request: %s", request)
 
 	setHeaders(writer, request.Header.Get("Origin"))
 
-	if conn == nil {
+	reply := make(chan string)
+	self.createChannel <- createConnection{reply}
+	if sessionId, ok := <-reply; !ok {
+		writer.WriteHeader(500)
+		writer.Write([]byte{})
+	} else {
+		fmt.Fprint(writer, sessionId)
+	}
+}
+
+type readHandler struct {
+	queryChannel chan queryConnection
+}
+
+func (self readHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	log.Printf("Handling Request: %s", request)
+
+	setHeaders(writer, request.Header.Get("Origin"))
+
+	conn, ok := findConnection(request.URL.Query()["sid"][0], self.queryChannel)
+	if !ok {
 		writer.WriteHeader(410)
-		_, err := writer.Write([]byte{}) // TODO: I think this is an allocation
-		if err != nil {
+		if _, err := writer.Write([]byte{}); err != nil {
 			log.Printf("Error writing zero bytes: %s", err)
 		}
+
+		return
 	}
 
 	var buffer [1024]byte
 
-	// Make sure that we call conn.SetDeadline(time.Now().Add(time.Duration(60)))
-	bytes, err := conn.Read(buffer[:])
-	if err != nil {
+	if bytes, err := conn.Read(buffer[:]); err != nil {
 		log.Printf("Error reading from ssh socket: %s", err)
-		conn = nil
+
+		// TODO: send close message to session manager
+		conn.Close()
+
 		writer.WriteHeader(410)
-		_, err := writer.Write([]byte{}) // TODO: I think this is an allocation
-		if err != nil {
+		if _, err := writer.Write([]byte{}); err != nil {
 			log.Printf("Error writing zero bytes: %s", err)
 		}
 	} else {
-		_, err := writer.Write([]byte(base64.URLEncoding.EncodeToString(buffer[:bytes])))
-		if err != nil {
+		if _, err := writer.Write(
+			[]byte(base64.URLEncoding.EncodeToString(buffer[:bytes]))); err != nil {
+
 			log.Printf("Error writing ssh bytes: %s", err)
+
+			// TODO: send close message to session manager
+			conn.Close()
 		}
 	}
 }
 
-func write(writer http.ResponseWriter, request *http.Request) {
+type writeHandler struct {
+	queryChannel chan queryConnection
+}
+
+func (self writeHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	log.Printf("Handling Request: %s", request)
 
 	setHeaders(writer, request.Header.Get("Origin"))
 
-	if conn == nil {
+	conn, ok := findConnection(request.URL.Query()["sid"][0], self.queryChannel)
+	if !ok {
 		writer.WriteHeader(410)
-		_, err := writer.Write([]byte{}) // TODO: I think this is an allocation
-		if err != nil {
+		if _, err := writer.Write([]byte{}); err != nil {
 			log.Printf("Error writing zero bytes: %s", err)
 		}
+
+		return
 	}
 
-	data := request.URL.Query().Get("data")
-
-	missingPadding := (4 - len(data)%4) % 4
-	for missingPadding > 0 {
-		data += "="
-		missingPadding -= 1
-	}
+	data := padData(request.URL.Query().Get("data"))
 
 	fmt.Printf("Decoding %d bytes: %s", len(data), data)
-	buffer, err := base64.URLEncoding.DecodeString(data)
-	if err != nil {
+	if buffer, err := base64.URLEncoding.DecodeString(data); err != nil {
 		log.Printf("Error decoding data: %s", err)
-		conn = nil
+
+		// TODO: send close message to session manager
+		conn.Close()
+
 		writer.WriteHeader(410)
 	} else {
 		_, err = conn.Write(buffer)
 		if err != nil {
 			log.Printf("Error writing to ssh socket: %s", err)
-			conn = nil
+
+			// TODO: send close message to session manager
+			conn.Close()
+
 			writer.WriteHeader(410)
 		} else {
 			writer.WriteHeader(200)
 		}
 	}
 
-	_, err = writer.Write([]byte{}) // TODO: I think this is an allocation
+	_, err := writer.Write([]byte{}) // TODO: I think this is an allocation
 	if err != nil {
 		log.Printf("Error writing zero bytes: %s", err)
 	}
@@ -137,4 +159,71 @@ func setHeaders(writer http.ResponseWriter, origin string) {
 	writer.Header().Set("Pragma", "no-cache")
 	writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	writer.Header().Set("Access-Control-Allow-Origin", origin)
+}
+
+func generateSessionId() (string, error) {
+	buffer := make([]byte, 18)
+	if _, err := rand.Read(buffer); err != nil {
+		fmt.Println("Error creating random session id: ", err)
+		return "", err
+	}
+
+	return base64.URLEncoding.EncodeToString(buffer), nil
+}
+
+func padData(data string) string {
+	missingPadding := (4 - len(data)%4) % 4
+	for missingPadding > 0 {
+		data += "="
+		missingPadding -= 1
+	}
+
+	return data
+}
+
+type createConnection struct {
+	reply chan string
+}
+
+type queryConnection struct {
+	sessionId string
+	reply     chan net.Conn
+}
+
+func sessionManager(create chan createConnection, query chan queryConnection) {
+	connections := make(map[string]net.Conn)
+
+	// TODO: We need to add at timer and check for stale connections
+
+	for {
+		select {
+		case msg := <-create:
+			if conn, err := net.Dial("tcp", "localhost:22"); err != nil {
+				log.Printf("Error creating ssh connection: %s", err)
+				close(msg.reply)
+			} else {
+				if sessionId, err := generateSessionId(); err != nil {
+					log.Printf("Error generating session id: %s", err)
+					close(msg.reply)
+				} else {
+					// TODO: check that sessionId doesn't exist
+					connections[sessionId] = conn
+					msg.reply <- sessionId
+				}
+			}
+		case msg := <-query:
+			if conn, ok := connections[msg.sessionId]; ok {
+				msg.reply <- conn
+			} else {
+				close(msg.reply)
+			}
+		}
+	}
+}
+
+func findConnection(sessionId string, queryChannel chan queryConnection) (net.Conn, bool) {
+	reply := make(chan net.Conn)
+	queryChannel <- queryConnection{sessionId, reply}
+	conn, ok := <-reply
+	return conn, ok
 }
